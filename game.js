e597df2471234ea9f0e635704cde946e707b7eb2
stav <sst46@nau.edu>
2019-10-07 17:25:50 -0700

    added mp3

--- game.js
+++ game.js
@@ -4,12 +4,18 @@ var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0xADD8E6});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+//var start_page = new PIXI.Container();
+//var end_page = new PIXI.Container();
 
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 PIXI.loader
   .add("assets.json")
   .load(ready);
+
+// PIXI.loader
+//   .add("dragonforce.mp3")
+//   .load(ready);
 
 function ready() {
   
@@ -30,6 +36,19 @@ function ready() {
   stage.addChild(character);
 }
 
+
+function mouseHandler(e)
+{
+  // dragonforce.play();
+  var new_x = Math.floor(Math.random() * 300) + 50;
+  var new_y = Math.floor(Math.random() * 300) + 50;
+  createjs.Tween.get(character.position).to({x: new_x, y: new_y}, 1000, createjs.Ease.bounceOut);
+}
+character.interactive = true;
+character.on("mousedown", mouseHandler);
+
+
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(stage);
@@ -38,22 +57,23 @@ animate();
 
 
 
-function keydownEventHandler(e) {
+// function keydownEventHandler(e) {
 
-  if (e.keyCode == 87) { // W key
-    character.position.y -= 10;
-  }
+//   if (e.keyCode == 87) { // W key
+//     character.position.y -= 10;
+//   }
 
-  if (e.keyCode == 83) { // S key
-    character.position.y += 10;
-  }
+//   if (e.keyCode == 83) { // S key
+//     character.position.y += 10;
+//   }
 
-  if (e.keyCode == 65) { // A key
-    character.position.x -= 10;
-  }
+//   if (e.keyCode == 65) { // A key
+//     character.position.x -= 10;
+//   }
 
-  if (e.keyCode == 68) { // D key
-    character.position.x += 10;
-  }
-}
-document.addEventListener('keydown', keydownEventHandler);
+//   if (e.keyCode == 68) { // D key
+//     character.position.x += 10;
+//   }
+
+// }
+// document.addEventListener('keydown', keydownEventHandler);
